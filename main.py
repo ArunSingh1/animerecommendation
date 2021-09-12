ff408a5694ee2625df6c1fcc074470d60642ea02
ArunSingh1 <arunsinghmailid@gmail.com>
2021-09-12 15:45:55 -0700

    streamlit final files

--- main.py
+++ main.py
@@ -2,16 +2,118 @@ from numpy.core.numeric import indices
 import pandas as pd
 import numpy as np
 import streamlit as st
-from utils import tfidf_matrix, indices, titles
+from utils import tfidf_matrix, indices, titles, content_recommender, df
+import base64
 
-st.title('Anime recommendation')
+
+#st.title('Anime recommendation')
+
+#st.set_page_config(layout="wide")
+
+st.markdown("""
+<style>
+.big-font {
+    font-family: 'Lobster', cursive; serif; font-size:75px !important;
+}
+
+# </style>
+# """, unsafe_allow_html=True)
+
+st.markdown('<p class="big-font">Anime recommendation</p>', unsafe_allow_html=True)
+
+
+##background iamge 
+# import base64
+
+# @st.cache(allow_output_mutation=True)
+# def get_base64_of_bin_file(bin_file):
+#     with open(bin_file, 'rb') as f:
+#         data = f.read()
+#     return base64.b64encode(data).decode()
+
+# def set_png_as_page_bg(png_file):
+#     bin_str = get_base64_of_bin_file(png_file)
+#     page_bg_img = '''
+#     <style>
+#     body {
+#     background-image: url("data:image/png;base64,%s");
+#     background-size: cover;
+#     }
+#     </style>
+#     ''' % bin_str
+    
+#     st.markdown(page_bg_img, unsafe_allow_html=True)
+#     return
+
+# set_png_as_page_bg('background.png')
+
+
+# title1 = titles[:100]
+
+option = None
+option = st.selectbox(
+    'Choose your favorite',
+   titles,index=0,key = None)
+st.write('Recommends Top 10 animes with similar story lines')
+
+
+if option is not None:
+
+    prediction = content_recommender(option, tfidf_matrix=tfidf_matrix, df=df, indices=indices)
+
+    for index, row in prediction.iterrows():
+        #print(row['title']),
+        st.subheader(row['title'])
+        #print(row['synopsis'])
+        # st.write('Synopsis')
+        st.write('Synopsis:::', """:book:""",  row['synopsis'])
+        st.write('Genres:::', """:musical_score:""",  row['genres'], " | ", 'Popularity:::', """:thumbsup:""", row['popularity'])
+        st.write('Rating:::', """:loudspeaker:""", row['rating'], " | ",""":movie_camera:""", row['type']," | ", row['status'])
+
+else:
+    pass
+
+
+
+# col1, col2, col3 = st.beta_columns([1,6,1])
+
+# with col1:
+#     st.write("")
+
+# with col2:
+#     st.image("https://i.imgflip.com/amucx.jpg")
+
+# with col3:
+#     st.write("")
+
+# url = "https://unsplash.com/photos/nKO_1QyFh9o"
+# #"https://images.unsplash.com/photo-1542281286-9e0a16bb7366"
+# page_bg_img = '''
+# <style>
+# body {
+# background-image: url("https://images.unsplash.com/photo-1542281286-9e0a16bb7366");
+# background-size: cover;
+# }
+# </style>
+# '''
+
+# st.markdown(page_bg_img, unsafe_allow_html=True)
+
+
 
 # option = st.selectbox(
 #     'How would you like to be contacted?',
 #    ('Email', 'Home phone', 'Mobile phone'))
 
+# def img_to_bytes(img_path):
+#     img_bytes = Path(img_path).read_bytes()
+#     encoded = base64.b64encode(img_bytes).decode()
+#     return encoded
 
-option = st.selectbox(
-    'How would you like to be contacted?',
-   titles)
-st.write('You eselected:', option)
+
+# header_html = "<img src='data:image/png;base64,{}' class='img-fluid'>".format(
+#     img_to_bytes("header.png")
+# )
+# st.markdown(
+#     header_html, unsafe_allow_html=True,
+# )
--- utils.py
+++ utils.py
@@ -3,7 +3,9 @@ import numpy as np
 
 #Load data 
 data = pd.read_csv("./data/anime_list.csv")
-df = data[['mal_id', 'title', 'synopsis', 'score', 'scored_by', 'rating']]
+#df = data[['mal_id', 'title', 'synopsis', 'score', 'scored_by', 'rating']]
+#df = data[['mal_id', 'title', 'synopsis', 'score', 'scored_by', 'rating','genres' ]]
+df = data[['mal_id', 'title', 'synopsis', 'score', 'scored_by', 'rating','genres', 'popularity','status','type','studios' ]]
 
 #create tfidf matrix
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -26,3 +28,25 @@ indices = pd.Series(df.index, index=df['title']).drop_duplicates()
 #print(indices)
 
 titles = data['title']
+
+# Function that takes in movie title as input and gives recommendations 
+def content_recommender(title, tfidf_matrix=tfidf_matrix, df=df, indices=indices):
+    # Obtain the index of the movie that matches the title
+    idx = indices[title]
+
+    # Get the pairwsie similarity scores of all movies with that movie
+    # And convert it into a list of tuples as described above
+    sim_scores = list(enumerate(linear_kernel(tfidf_matrix[idx], tfidf_matrix).flatten()))
+
+    # Sort the movies based on the cosine similarity scores
+    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+
+    # Get the scores of the 10 most similar movies. Ignore the first movie.
+    sim_scores = sim_scores[1:11]
+
+    # Get the movie indices
+    movie_indices = [i[0] for i in sim_scores]
+
+    # Return the top 10 most similar movies df[['title','synopsis']].iloc[movie_indices] 
+    # df['title'].iloc[movie_indices]
+    return df[['title','synopsis','genres', 'popularity','status','type','studios','rating' ]].iloc[movie_indices]
